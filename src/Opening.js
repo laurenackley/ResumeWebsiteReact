de837470ec1fa0d41642a1d00e18105944f96e91
laurenackley <lackley127@gmail.com>
2023-03-23 12:15:21 -0500

    Deployed React App to Amazon S3 and changed icon

--- src/Opening.js
+++ src/Opening.js
@@ -1,54 +1,66 @@
-import Carousel from 'react-bootstrap/Carousel';
-
+import Carousel from "react-bootstrap/Carousel";
 
 const Opening = () => {
-    return (
-        <div className="opening">
-            <h2>Opening</h2>
-            {IndividualIntervalsExample()}
-        </div>
-    )
-}
+  return (
+    <div className="opening">
+      <h2>Thanks for Visiting!</h2>
+      <p>
+        I worked as a Technical Support Specialist for 3 years before deciding
+        to make a career change and pursue my passion of programming.
+      </p>
+      {IndividualIntervalsExample()}
+    </div>
+  );
+};
 export default Opening;
 
 function IndividualIntervalsExample() {
-    return (
-      <Carousel className="carousel">
-        <Carousel.Item interval={1000}>
-          <img
-            className="d-flex justify-content-center align-items-center w-auto h-100 m-auto"
-            src="https://i.imgur.com/lgpMZ1H.jpg"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={500}>
-          <img
-            className="d-flex justify-content-center w-auto h-100 m-auto"
-            src="https://i.imgur.com/VwIqFYH_d.jpg?maxwidth=520&shape=thumb&fidelity=high"
-            alt="Second slide"
-          />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-flex justify-content-center w-auto h-100 m-auto"
-            src="https://i.imgur.com/ib6ztwj.jpg"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    );
-  }
+  return (
+    <Carousel className="carousel">
+      <Carousel.Item interval={5000}>
+        <img
+          className="d-flex justify-content-center align-items-center w-auto h-100 m-auto"
+          src="https://i.imgur.com/lgpMZ1H.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          {/* <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={5000}>
+        <img
+          className="d-flex justify-content-center w-auto h-100 m-auto"
+          src="https://i.imgur.com/VwIqFYH_d.jpg?maxwidth=520&shape=thumb&fidelity=high"
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Jack and Jules</h3>
+          <p></p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={5000}>
+        <img
+          className="d-flex justify-content-center w-auto h-100 m-auto"
+          src="https://i.imgur.com/ib6ztwj.jpg"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Elvira</h3>
+          {/* <p>This is my 2 year old Anatolian Shepard Great Pyrenees mix dog, Elvira.  </p> */}
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={5000}>
+        <img
+          className="d-flex justify-content-center w-auto h-100 m-auto"
+          src="https://i.imgur.com/xGi79Ca.jpg"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Turtle the Cat</h3>
+          {/* <p>This is my 2 year old Anatolian Shepard Great Pyrenees mix dog, Elvira.  </p> */}
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
